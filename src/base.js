3139ae8242d53a36323b7503b7fbbb48c366340d
PetxCode <brighterdayscodelab@gmail.com>
2020-12-16 10:16:10 +0100

    first commit

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyCLi0luMCVAHIyN-OE_pxSbi1e6XC2yc84",
+  authDomain: "ajegunle-developers.firebaseapp.com",
+  projectId: "ajegunle-developers",
+  storageBucket: "ajegunle-developers.appspot.com",
+  messagingSenderId: "582961766248",
+  appId: "1:582961766248:web:9820da8c1acb5a152f00dd",
+});
